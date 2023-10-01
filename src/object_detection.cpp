3def73295438c1aebea2001ee988e02142d70d5a
David Davila <adavidguamandavila@gmail.com>
2023-10-01 10:12:54 -0230

    initial commit: linking local to remote

---
+++ src/object_detection.cpp
@@ -0,0 +1,65 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+
+
+class Image_Finder{
+public:
+    
+    Image_Finder(): nh_("") {
+        //Subsribe to camera topic publishing data
+        sub_img_ = nh_.subscribe("/camera/color/image_raw", 1, &Image_Finder::imageCallback, this);
+
+    }
+
+    //Function to continue the life of the node
+    void spin(){
+        ros::Rate rate(2); //10Hz
+        while(ros::ok()){
+            ros::spinOnce();
+            rate.sleep();
+        }
+    }
+
+
+private:
+    ros::NodeHandle nh_;
+    ros::Subscriber sub_img_;
+
+    sensor_msgs::Image img_msg_;
+
+    std::vector<uint8_t> image_data_matrix;
+    uint32_t image_height;
+    uint32_t image_width;
+    std::string encoding;
+
+
+    void imageCallback(const sensor_msgs::Image::ConstPtr& msg){
+        //Access the image data from the message
+        image_data_matrix = msg->data;
+
+        //Acces image dimensions
+        image_height = msg->height;
+        image_width = msg->width;
+
+        //Access image encoding
+        std::string encoding = msg->encoding;
+
+        //Save image message
+        img_msg_ = *msg;
+
+        // ROS_INFO("Recived an image with width="<< image_width << "and height" << image_height);
+
+    }
+
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "object_detection");
+
+
+    Image_Finder image_finder;
+    image_finder.spin();
+
+    return 0;
+}
