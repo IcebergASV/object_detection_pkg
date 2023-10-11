f88279988fe699866463a9e137cd55ef416f9c2d
David Davila <adavidguamandavila@gmail.com>
2023-10-11 20:04:48 -0230

    Simple Publisher created and working

--- src/object_detection.cpp
+++ src/object_detection.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <ros/console.h>
+#include <geometry_msgs/Vector3.h>
 
 
 class Image_Finder{
@@ -9,13 +10,15 @@ public:
     Image_Finder(): nh_("") {
         //Subsribe to camera topic publishing data
         sub_img_ = nh_.subscribe("/camera/color/image_raw", 1, &Image_Finder::imageCallback, this);
+        publisher = nh_.advertise<geometry_msgs::Vector3>("/raw_image_copy", 1);;
     }
 
     //Function to continue the life of the node
     void spin(){
-        ros::Rate rate(2); //10Hz
-        while(ros::ok()){
+        ros::Rate rate(2); // 2Hz
+        while (ros::ok()) {
             ros::spinOnce();
+            publishImageInfo(); // Publish image information
             rate.sleep();
         }
     }
@@ -24,6 +27,7 @@ public:
 private:
     ros::NodeHandle nh_;
     ros::Subscriber sub_img_;
+    ros::Publisher publisher;
 
     sensor_msgs::Image img_msg_;
 
@@ -46,8 +50,24 @@ private:
         //Save image message
         img_msg_ = *msg;
 
-        // Use ROS_DEBUG_STREAM for debugging
-        ROS_DEBUG_STREAM("Received an image with width=" << image_width << " and height=" << image_height);
+        // // Use ROS_DEBUG_STREAM for debugging
+        // ROS_DEBUG_STREAM("Received an image with width=" << image_width << " and height=" << image_height);
+
+        geometry_msgs::Vector3 prop_coords_msg;
+
+    }
+
+    void publishImageInfo() {
+        // Create a Vector3 message to publish image info
+        geometry_msgs::Vector3 msg;
+        msg.x = static_cast<double>(image_width);
+        msg.y = static_cast<double>(image_height);
+        // Assuming you want to publish encoding information in the z field
+        // Modify this part if you want to publish the image data
+        msg.z = 0.0; // You can replace 0.0 with encoding information
+
+        // Publish the message
+        publisher.publish(msg);
     }
 };
 
